aa02a1333c96a6f18ce97acaaae2ad30ec2e9f25
Samnoon <36998653+samnoon1971@users.noreply.github.com>
2019-11-30 14:39:17 +0600

    Add files via upload

---
+++ src/StreamPractices/EvenOdd.java
@@ -0,0 +1,17 @@
+package StreamPractices;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EvenOdd {
+    public static void main(String[] args){
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        List<Integer> even =
+                numbers.stream()
+                .filter(e->e%2==0)
+                .collect(Collectors.toList());
+        even.forEach(System.out::println);
+    }
+}
